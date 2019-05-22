c31d2115b416afe1603121495e9f3a72c4bcba18
okeyja_win32 <dengyijian163@163.com>
2019-05-22 20:54:39 +0800

    init: init with empty project

---
+++ src/main/java/net/cakecdn/api/user/CakeApiUserApplication.java
@@ -0,0 +1,13 @@
+package net.cakecdn.api.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeApiUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CakeApiUserApplication.class, args);
+    }
+
+}
